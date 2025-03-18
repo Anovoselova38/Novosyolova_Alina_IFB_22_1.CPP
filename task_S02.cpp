1398e2fe249fa0717f0f655f873df7a88dc5d14f
Anovoselova38 <147522102+Anovoselova38@users.noreply.github.com>
2025-03-18 21:03:17 +0800

    Add files via upload

---
+++ task_S02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+/*
+S02: Демонстрация работы операторов деления нацело и
+определения остатка от деления
+*/
+
+main() {
+    int number_1 = 10;
+    int number_2 = 3;
+
+    std::cout << number_1  / number_2 << std::endl;
+    std::cout << number_1 % number_2 << std::endl;
+}
