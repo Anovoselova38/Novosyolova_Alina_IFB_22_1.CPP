9fc382bed62ce15262c7765ece1da2b0ba6e5d47
Anovoselova38 <147522102+Anovoselova38@users.noreply.github.com>
2025-03-18 21:01:56 +0800

    Add files via upload
    S01: Создание 4 переменных разного типа
    и демонстрация работы арифметических операторов

---
+++ task_S01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/*
+S01: Создание 4 переменных разного типа
+и демонстрация работы арифметических операторов
+*/
+
+main() {
+    int number_1 = 10;
+    double number_2 = 5.5;
+    float number_3 = 2.5f;
+    long number_4 = 20;
+
+    std::cout << number_1 + number_2 << std::endl;
+    std::cout << number_2 - number_3 << std::endl;
+    std::cout <<  number_3 * number_4 << std::endl;
+    std::cout <<  number_4 / number_1 << std::endl;
+}
