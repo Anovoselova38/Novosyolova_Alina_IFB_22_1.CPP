c2fe4f73e5cddb7a96e71a5d2c77760a794346c3
Anovoselova38 <147522102+Anovoselova38@users.noreply.github.com>
2025-03-22 18:13:56 +0800

    Update task_21.cpp

--- task_21.cpp
+++ task_21.cpp
@@ -5,9 +5,8 @@ int main(){
     ofstream file_to_write("output");
     setlocale(0,"Russian");
     int A,B;
-    cout << "Ââåäèòå ÷èñëà" << endl;
+    cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ Ñ‡Ð¸ÑÐ»Ð°" << endl;
     cin >> A >> B;
-    file_to_write << "Óìíîæèëè íà 3" << endl;
         for (int i = A; i <= B; ++i){
         file_to_write << i * 3 << endl ;
 
