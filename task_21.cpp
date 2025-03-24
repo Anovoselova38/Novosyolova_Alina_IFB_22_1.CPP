75d697ff6d338c418600861ca9d0b2df2941c1d3
Anovoselova38 <147522102+Anovoselova38@users.noreply.github.com>
2025-03-24 14:24:40 +0800

    Add files via upload

--- task_21.cpp
+++ task_21.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <fstream>
-using namespace std;
-int main(){
-    ofstream file_to_write("output");
-    setlocale(0,"Russian");
-    int A,B;
-    cout << "Р’РІРµРґРёС‚Рµ С‡РёСЃР»Р°" << endl;
-    cin >> A >> B;
-        for (int i = A; i <= B; ++i){
-        file_to_write << i * 3 << endl ;
 
-}
+using namespace std;
+
+int main() {
+    setlocale(0, "Russian");
+
+    // Получить от пользователя 2 числа
+    double A, B;
+    cout << "Введите два числа A и B: ";
+    cin >> A >> B;
+
+    // Открываем файл для записи
+    ofstream file_to_write("output.txt");
+    for (int i = A; i <= B;++i){
+        // Записываем числа в файл
+    file_to_write << i * 3 << endl;
+    }
+
+    // Закрываем файл
     file_to_write.close();
     return 0;
 }
