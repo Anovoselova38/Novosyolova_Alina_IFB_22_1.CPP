0a118118a416360a70f1b9550c9bc685d480b1e9
Anovoselova38 <147522102+Anovoselova38@users.noreply.github.com>
2025-03-28 15:44:02 +0800

    Add files via upload

--- task_S41.cpp
+++ task_S41.cpp
@@ -2,19 +2,17 @@
 которая принимает два дробных числа
 и возвращает квадрат их суммы
 */
-#include <io
 #include <iostream>
-#include <cmath> 
+#include <cmath>
 using namespace std;
 
 double squared_sum(double number_1, double number_2){
-     return pow(number_1 + number_2, 2);
+    double sum = number_1 + number_2 ;
+    return (sum * sum);
 }
 int main(){
     setlocale(0,"Russian");
     double result = squared_sum(12, 12);
-    cout << "Êâàäðàò ñóììû: " << result << endl;
-
-
+    cout << result << endl;
     return 0;
 }
