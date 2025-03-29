fadd968a8ad028840038aed4790b90f86e8c8c07
Anovoselova38 <147522102+Anovoselova38@users.noreply.github.com>
2025-03-29 13:02:01 +0800

    Add files via upload

---
+++ task_S42.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+Sort(int numbers[], int size_numbers) {
+    for (int i = 0; i < size_numbers - 1; ++i) {
+        for (int j = 0; j < size_numbers - i - 1; ++j) {
+            if (numbers[j] < numbers[j + 1]) { // по убываниию
+                // Меняем элементы местами
+                int temporary = numbers[j];  //для  сохранения элемента при замене положения
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = temporary;
+            }
+        }
+    }
+}
+
+int main() {
+    setlocale(0,"Russian");
+    int numbers[] = {10, 2, 8, 4, 6, 5, 7, 3, 9, 1};
+    int size_numbers = sizeof(numbers) / sizeof(numbers[0]);
+
+    Sort(numbers, size_numbers);
+
+    cout << "После сортировки: ";
+    for (int i = 0; i < size_numbers; ++i) {
+        cout << numbers[i] << " ";
+    }
+
+    return 0;
+}
